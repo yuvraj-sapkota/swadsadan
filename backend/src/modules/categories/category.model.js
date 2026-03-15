147b075fd99e816110506195c2208551d135ae53
nabinnew <nabin.rock044@gmail.com>
2026-03-16 00:10:48 +0545

    fiz hotelid  function

--- backend/src/modules/categories/category.model.js
+++ backend/src/modules/categories/category.model.js
@@ -6,7 +6,7 @@ const categorySchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Restaurant",
       required: true,
-      unique: true  
+      
       },
 
     name: {
